0d0e63274b503f91ecfada14f521efaf4445bb08
mahidulkabir <mahidulkabir8@gmail.com>
2025-09-18 12:46:06 +0600

    aded some more react code

--- vite-project/src/FirstPage.jsx
+++ vite-project/src/FirstPage.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
 
 const FirstPage = () => {
-    return (
-        <div>
-            <h1>First Page</h1>
+  return (
+    <div>
+   
+      <h1>This is for a bootstrap form </h1>
+      {/* bootstrap form start  */}
+      <div className="row">
+        <div className="col-6">
+             <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
         </div>
-    );
+        <div class="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+        </div>
+         
+      </div>
+    
+    </div>
+  );
 };
 
 export default FirstPage;
